8af42765c9575fff400d2ce70ebb488e6d0b3a8b
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-09-09 10:02:20 -0400

    added get endpoint for scores

--- scores.api.js
+++ scores.api.js
@@ -4,10 +4,13 @@ const pool = require("./connection");
 
 function getScoreboard(req, res) {
     pool.query("select * from scores order by score desc, username").then((result) => {
+        console.log("get for scores works");
         res.send(result.rows);
         // console.log("Get scores works");
     });
 }
+
+scoresRoutes.get("/scores", getScoreboard);
 
 
 scoresRoutes.post("/scores", (req, res) => {
@@ -16,6 +19,7 @@ scoresRoutes.post("/scores", (req, res) => {
         req.body.score
         ])
             .then(() => {
+            console.log("post scores works");
             getScoreboard(req, res);
             // console.log("Post works");
         });
