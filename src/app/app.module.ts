019eb35dbdd3aae0c9e8ae80dd1b8c026cdb83e2
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-09-11 10:45:05 -0400

    POKEBALL ANIMATION WORKS YAYAYY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { PokequizService } from './pokequiz.service';
-import { QuizpageComponent } from './quizpage/quizpage.component';
-import { RouterModule, Routes } from '@angular/router';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { InstructionsComponent } from './instructions/instructions.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { FormsModule } from '@angular/forms';
-import { ResultspageComponent } from './resultspage/resultspage.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { PokequizService } from "./pokequiz.service";
+import { QuizpageComponent } from "./quizpage/quizpage.component";
+import { RouterModule, Routes } from "@angular/router";
+import { LandingpageComponent } from "./landingpage/landingpage.component";
+import { InstructionsComponent } from "./instructions/instructions.component";
+import { ScoreboardComponent } from "./scoreboard/scoreboard.component";
+import { FormsModule } from "@angular/forms";
+import { ResultspageComponent } from "./resultspage/resultspage.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 const appRoutes: Routes = [
   { path: "home", component: LandingpageComponent },
   { path: "instructions", component: InstructionsComponent },
-  { path: "quiz", component: QuizpageComponent},
+  { path: "quiz", component: QuizpageComponent },
   { path: "results", component: ResultspageComponent },
   { path: "scores", component: ScoreboardComponent },
-  { path: '', redirectTo: "/home", pathMatch: "full"},
+  { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
 @NgModule({
@@ -33,9 +34,10 @@ const appRoutes: Routes = [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [PokequizService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
