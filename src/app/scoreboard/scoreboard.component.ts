b62bb3b4144e05a5771e981a5f1592f0f71fa6d8
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-09-13 14:42:28 -0400

    can get scores by grade level

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { PokequizService } from '../pokequiz.service';
+import { Component, OnInit } from "@angular/core";
+import { PokequizService } from "../pokequiz.service";
 
 @Component({
-  selector: 'app-scoreboard',
-  templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.css']
+  selector: "app-scoreboard",
+  templateUrl: "./scoreboard.component.html",
+  styleUrls: ["./scoreboard.component.css"]
 })
 export class ScoreboardComponent implements OnInit {
   scoreboard: any;
+  gradeLevel: string;
 
   constructor(private pokequizService: PokequizService) {}
 
   ngOnInit() {
-    this.pokequizService.getScoreboard().subscribe(response => {
+    this.gradeLevel = this.pokequizService.setUserGradeLevel();
+    this.pokequizService.getScoreboard(this.gradeLevel).subscribe(response => {
       this.scoreboard = response;
       console.log(this.scoreboard);
     });
-}
-
+  }
 }
